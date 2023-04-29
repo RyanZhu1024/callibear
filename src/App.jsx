adb6f9553018eb80a3307fb4fb9289a7452323b8
Ryan Zhu <ryzhu@linkedin.com>
2023-04-29 15:57:47 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import './App.css'
+import data from './data';
+
+function App() {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [selectedItem, setSelectedItem] = useState(null);
+  const handleChangeSearch = e => {
+    setSearchKeyword(e.target.value);
+  };
+  const handleSelectItem = (item) => () => {
+    setSelectedItem(item);
+  }
+  return (
+    <>
+      <form>
+        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+          </div>
+          <input
+            onChange={handleChangeSearch}
+            value={searchKeyword}
+            type="search"
+            id="default-search"
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search names..."
+            required
+          />
+        </div>
+      </form>
+      <div className='mt-6'>
+        <ul className="mb-8 space-y-4 text-left text-gray-500 dark:text-gray-400">
+          {data.filter((item) => item.name.toLowerCase().indexOf(searchKeyword.toLowerCase()) > -1).map(item => (
+            <li
+              onClick={handleSelectItem(item)}
+              key={item.name}
+              className="h-12 flex items-center p-8 cursor-pointer hover:bg-slate-50 text-2xl"
+            >
+              <span>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {selectedItem ?
+        <div tabIndex="-1" aria-hidden="true" data-modal-backdrop="static" className="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex">
+          <div className="relative w-full max-w-2xl max-h-full mx-auto">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  {selectedItem.name}
+                </h3>
+                <button onClick={() => setSelectedItem(null)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                  <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+              <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div> : null}
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export default [
+  { name: 'Callibear milk tea' },
+  { name: 'Mengo tea' },
+  { name: 'Mengo refresher' },
+  { name: 'Peach refresher' },
+]
